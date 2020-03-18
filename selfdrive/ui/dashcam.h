8a325fea79722226c91bc053bc42a8ffa8ac1ea3
hsyoo2000 <61792825+hsyoo2000@users.noreply.github.com>
2020-03-18 11:40:27 +0900

    Add files via upload

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -141,7 +141,6 @@ void start_capture() {
   struct tm tm = get_time_struct();
   snprintf(filename,sizeof(filename),"%04d%02d%02d-%02d%02d%02d.mp4", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
   snprintf(cmd,sizeof(cmd),"screenrecord --size 1280x720 --bit-rate 5000000 %s/%s&",videos_dir,filename);
-  //snprintf(cmd,sizeof(cmd),"screenrecord --size 960x540 --bit-rate 5000000 %s/%s&",videos_dir,filename);
   strcpy(filenames[captureNum],filename);
 
   printf("Capturing to file: %s\n",cmd);
@@ -340,6 +339,7 @@ void dashcam( UIState *s, int touch_x, int touch_y ) {
   if (s->scene.v_ego > 2.1 && captureState == CAPTURE_STATE_NOT_CAPTURING && !s->scene.engaged) {
     start_capture();
   } else if (s->scene.v_ego < 1.5 && !s->scene.engaged) {
+    
     stop_capture();
   }
   s->scene.recording = (captureState != CAPTURE_STATE_NOT_CAPTURING);
